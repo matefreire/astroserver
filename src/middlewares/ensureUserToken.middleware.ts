6fe518843364408fa79dc893063d536039ad4e74
karol <karolrds01@gmail.com>
2024-03-08 14:45:16 -0400

    feat: add token midl

---
+++ src/middlewares/ensureUserToken.middleware.ts
@@ -0,0 +1,23 @@
+import { AppError } from "@/error";
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+
+export const ensureUserToken = (req: Request, res: Response, next: NextFunction) => {
+
+    const token: string | undefined = req.headers.authorization
+
+    if(!token){
+        throw new AppError("Token is missing", 401)
+    }
+
+    const oToken = token.split('')[1]
+
+    jwt.verify(oToken, process.env.SECRET_KEY!, (err:any, decoded:any) => {
+        if(err)throw new AppError(err.message)
+        
+        res.locals.isAdmin = decoded.isAdmin
+        res.locals.email = decoded.email
+
+        next()
+    })
+}
